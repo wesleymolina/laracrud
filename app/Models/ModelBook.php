b830ede3f1d031e38c8cf3e3528f7f1ceb09be4f
Wesley Molina <wesley.molina@doptex.com>
2020-07-14 17:15:21 -0300

    .

---
+++ app/Models/ModelBook.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelBook extends Model
+{
+    protected $table='book';
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.template')
+@section('conteudo')
+
+    <h1 class="text-center">Laracrud</h1>
+@endsection
